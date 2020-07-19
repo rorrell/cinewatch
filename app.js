06b4b431528c16a2fdceaba37692878e0e5d76ec
rorrell <Ykt!lk1t>
2020-07-18 19:18:43 -0600

    Added hard-coded movie data and everything needed for basic display and search.

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path');
 const mysql = require('./dbcon.js');
 const bodyParser = require('body-parser');
+const movies = require('./movies')
 
 // constants
 const port = 9716;
 const app = express();
 
 // set up handlebars
-app.set('view engine', 'handlebars');
-app.engine('handlebars', handlebars({
+var hbs = exphbs.create({
     layoutsDir: path.join(__dirname, 'views/layouts'),
     defaultLayout:'main',
-    partialsDir: path.join(__dirname, 'views/partials')
-}));
+    partialsDir: path.join(__dirname, 'views/partials'),
+    helpers: {
+        getAge: function (dobString) {
+            if(dobString == null) {
+                return "Unknown";
+            }
+            let currentDate = new Date();
+            //clear the time from the current date so it's not considered in calculations
+            currentDate.setHours(0, 0, 0, 0);
+            let dob = new Date(dobString);
+            let years = currentDate.getFullYear() - dob.getFullYear();
+            if((currentDate.getMonth() < dob.getMonth()) || (currentDate.getMonth() === dob.getMonth() && currentDate.getDate() < dob.getDate())) {
+                years -= 1;
+            }
+            return years;
+        },
+        yearOnly: function (dateString) {
+            let date = new Date(dateString);
+            date.setHours(0, 0, 0, 0);
+            return date.getFullYear();
+        }
+    }
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //set up body parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,11 +46,33 @@ app.use(bodyParser.json());
 // set up file location for static files
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('/', (req, res, next) => {
+app.get('/', function (req, res, next) {
     let context = {
         message: "Welcome!"
     };
     res.render('home', context);
+});
+
+app.get('/dashboard', function (req, res, next) {
+    let context = {
+        movies: movies,
+        isDashboard: true
+    };
+    res.render('dashboard', context);
+});
+
+app.get('/movie/:id', function (req, res, next) {
+    let context = movies.find(mov => mov.movieID.toString() === req.params.id);
+    res.render('movie', context);
+});
+
+app.get('/movies', function (req, res, next) {
+    let results = movies.filter(mov => mov.name.toLowerCase().includes(req.query.q.toLowerCase()));
+    let context = {
+        movies: results,
+        isDashboard: false
+    };
+    res.render('dashboard', context);
 });
 
 // start app
---
+++ movies.js
@@ -0,0 +1,135 @@
+module.exports = [
+    {
+        movieID: 1,
+        name: "Big Miracle",
+        releaseDate: "2012-02-03",
+        averageCriticRating: "6.5",
+        synopsis: "In small-town Alaska, a news reporter recruits his ex-girlfriend - a Greenpeace volunteer - on a campaign to save a family of gray whales trapped by rapidly forming ice in the Arctic Circle.",
+        actors: [
+            {
+                firstName: "Drew",
+                lastName: "Barrymore",
+                dob: "1975-02-22 00:00:00"
+            },
+            {
+                firstName: "John",
+                lastName: "Krasinski",
+                dob: "1979-10-20 00:00:00"
+            },
+            {
+                firstName: "John",
+                lastName: "Pingayak",
+                dob: null
+            }
+        ],
+        genres: [
+            {
+                title: "Biography"
+            },
+            {
+                title: "Drama"
+            },
+            {
+                title: "Family"
+            },
+            {
+                title: "Romance"
+            }
+        ],
+        awards: []
+    },
+    {
+        movieID: 2,
+        name: "Jurassic Park",
+        releaseDate: "1993-06-11",
+        averageCriticRating: "8.1",
+        synopsis: "A pragmatic paleontologist visiting an almost complete theme park is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.",
+        actors: [
+            {
+                firstName: "Sam",
+                lastName: "Neill",
+                dob: "1947-09-14 00:00:00"
+            },
+            {
+                firstName: "Laura",
+                lastName: "Dern",
+                dob: "1967-02-10 00:00:00"
+            },
+            {
+                firstName: "Jeff",
+                lastName: "Goldblum",
+                //dob: "1952-10-22 00:00:00"
+                dob: "1952-07-18"
+            }
+        ],
+        genres: [
+            {
+                title: "Action"
+            },
+            {
+                title: "Adventure"
+            },
+            {
+                title: "Sci-Fi"
+            },
+            {
+                title: "Thriller"
+            }
+        ],
+        awards: [
+            {
+                name: "Oscar: Best Sound",
+                dateAwarded: "1994-03-21 00:00:00"
+            },
+            {
+                name: "Oscar: Best Sound Effect Editing",
+                dateAwarded: "1994-03-21 00:00:00"
+            },
+            {
+                name: "Oscar: Best Visual Effects",
+                dateAwarded: "1994-03-21 00:00:00"
+            }
+        ]
+    },
+    {
+        movieID: 3,
+        name: "Dante's Peak",
+        releaseDate: "1997-02-07",
+        averageCriticRating: "5.9",
+        synopsis: "A vulcanologist arrives at a countryside town recently named the second most desirable place to live in America, and discovers that the long dormant volcano, Dante's Peak, may wake up at any moment.",
+        actors: [
+            {
+                firstName: "Pierce",
+                lastName: "Brosnan",
+                dob: "1953-05-16 00:00:00"
+            },
+            {
+                firstName: "Linda",
+                lastName: "Hamilton",
+                dob: "1956-09-26 00:00:00"
+            },
+            {
+                firstName: "Jamie",
+                lastName: "Smith",
+                dob: "1987-04-10 00:00:00"
+            }
+        ],
+        genres: [
+            {
+                title: "Action"
+            },
+            {
+                title: "Adventure"
+            },
+            {
+                title: "Thriller"
+            }
+        ],
+        awards: [
+            {
+                name: "ASCAP: Top Box Office Films",
+                dateAwarded: "1998-10-22 00:00:00"
+            }
+        ]
+    }
+];

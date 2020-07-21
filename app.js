719bb18d998c8d64aff143cfd87bfd076d938863
rorrell <Ykt!lk1t>
2020-07-20 22:13:16 -0600

    Final heroku compliance changes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
